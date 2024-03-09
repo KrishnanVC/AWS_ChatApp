970156c65785d2393092e804c08252b00a81a850
Krishnan VC <krishnan.vcg@gmai.com>
2024-03-09 20:11:11 +0530

    Add response from api gateway

--- src/handlers/connect.py
+++ src/handlers/connect.py
@@ -5,7 +5,11 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     logger.info(event)
+    connection_id = event["requestContext"]["connectionId"]
     return {
         "statusCode": 200,
-        "body": "Connection successful"
+        "body": {
+          "message": "Connection successful",
+          "connection_id": connection_id
+        }    
     }
